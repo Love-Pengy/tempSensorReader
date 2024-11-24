5fdbaca4ff9ac387b5e65c64acbf5a132c264e01
Love-Pengy <afrazierboy2001@gmail.com>
2024-11-24 09:58:53 -0500

    [REFACTOR] fix messed up project tree

--- tempSensorReader.srcs/sim_1/imports/Downloads/TWICtl_testbench.vhd
+++ tempSensorReader.srcs/sim_1/imports/Downloads/TWICtl_testbench.vhd
@@ -17,19 +17,13 @@ architecture behavior of TWI_testbench is
 			end loop;
 	end waitclocks;
 
-  signal CLK_sig, RESET_sig, START_sig, SRST_sig : std_logic := '0';					-- signals to connect to the TWI
-  signal SCL_sig, MSG_I_sig, STB_I_sig, DONE_O_sig, ERR_O_sig, SDA_sig : std_logic; 
+  signal CLK_sig, RESET_sig, SRST_sig : std_logic := '0';					-- signals to connect to the TWI
+  signal SCL_sig, MSG_I_sig, STB_I_sig, DONE_O_sig, ERR_O_sig, SDA_sig, slow_clk : std_logic; 
   signal A_I_sig, D_I_sig,  D_O_sig : STD_LOGIC_VECTOR (7 downto 0);
-  signal startClk_sig : std_logic := '0';
   signal DATA_OUT_sig    : STD_LOGIC_VECTOR(15 downto 0);	-- 16-bit "result" of 2-byte read from TMP
+  
+  constant Tperiod : time := 10 ns;         -- 100 MHz clock
 
-  constant addrAD2	 : STD_LOGIC_VECTOR(6 downto 0) := b"1001011";	-- TWI address for the TMP
-  constant read_Bit  : STD_LOGIC := '1';
-  constant write_Bit : STD_LOGIC := '0';
-  
-  
-  constant Tperiod : time := 10 ns;						-- 100 MHz clock
-  constant startPeriod : time := 2000 ms; -- 2 hz clock 
   
   begin
   
@@ -39,11 +33,6 @@ architecture behavior of TWI_testbench is
       begin
         CLK_sig <= not CLK_sig after Tperiod/2;
     end process;
-    
-    process(startClk_sig)
-        begin
-            startCLK_sig <= not startClk_sig after startPeriod/2;
-    end process; 
   
 	-- This process simulates the state machine that drives the TWI controller to perform the bus
 	-- master operations. These include a configuration register write, followed by a two-byte
@@ -53,15 +42,16 @@ architecture behavior of TWI_testbench is
     -- STB_I --strobe to start TWI bus cycle
     -- A_I   --address input bus
     -- D_I   --data input bus
-
-
+	
+	  CLK_DIV : ENTITY work.clock_divider(behavior)
+                 PORT MAP(mclk => CLK_sig, sclk => slow_clk);
 
 	master_stimulus : ENTITY work.ADT_CSM(Behavioral)
-		PORT MAP(START => startClk_sig, 
+		PORT MAP(START => slow_clk, 
 		        RESET => RESET_sig, 
 		        SRST => SRST_sig,
 		        DATA_OUT => DATA_OUT_sig,  
-		        clk => CLK_sig, 
+		        CLK => CLK_sig, 
 		        STB_I => STB_I_sig,
 				MSG_I => MSG_I_sig,
 				A_I => A_I_sig, 
@@ -70,7 +60,7 @@ architecture behavior of TWI_testbench is
 				ERR_O => ERR_O_sig, 
 				D_O => D_O_sig 
 				);
-				
+
 	-- This process the TMP slave device on the TWI bus. It drives the SDA signal to '0' at the appropriate
 	-- times to furnish an "ACK" signal to the TWI master device and '0' and 'H' at appropriate times to 
 	-- simulate the data being returned from the TMP over the TWI bus.
@@ -80,7 +70,7 @@ architecture behavior of TWI_testbench is
 		SDA_sig <= 'H';						-- not driven
 		SCL_sig <= 'H';						-- not driven
 
-										-- address write
+										-- First address write
 		waitclocks(SCL_sig, 9);			-- wait for transmission time
 		SDA_sig <= '0';
 		waitclocks(SCL_sig, 1);			-- wait for ack time
@@ -124,6 +114,51 @@ architecture behavior of TWI_testbench is
 		waitclocks(SCL_sig, 1);
 		SDA_sig <= 'H';
 
+
+										-- Second address write
+		waitclocks(SCL_sig, 9);			-- wait for transmission time
+		SDA_sig <= '0';
+		waitclocks(SCL_sig, 1);			-- wait for ack time
+		SDA_sig <= 'H';
+
+		SDA_sig <= 'H';					-- MSB (upper byte)
+		waitclocks(SCL_sig, 1);	
+		SDA_sig <= '0';
+		waitclocks(SCL_sig, 1);
+		SDA_sig <= 'H';
+		waitclocks(SCL_sig, 1);
+		SDA_sig <= '0';
+		waitclocks(SCL_sig, 1);
+		SDA_sig <= '0';
+		waitclocks(SCL_sig, 1);
+		SDA_sig <= '0';
+		waitclocks(SCL_sig, 1);
+		SDA_sig <= '0';
+		waitclocks(SCL_sig, 1);
+		SDA_sig <= '0';					-- LSB
+		waitclocks(SCL_sig, 1);
+		SDA_sig <= 'H';					-- Release bus
+
+		
+		waitclocks(SCL_sig, 1);			
+		SDA_sig <= '0';					-- MSB (lower byte)
+		waitclocks(SCL_sig, 1);
+		SDA_sig <= 'H';
+		waitclocks(SCL_sig, 1);
+		SDA_sig <= '0';
+		waitclocks(SCL_sig, 1);
+		SDA_sig <= 'H';
+		waitclocks(SCL_sig, 1);
+		SDA_sig <= 'H';
+		waitclocks(SCL_sig, 1);
+		SDA_sig <= '0';
+		waitclocks(SCL_sig, 1);
+		SDA_sig <= '0';
+		waitclocks(SCL_sig, 1);
+		SDA_sig <= '0';					-- LSB (lower byte)
+		waitclocks(SCL_sig, 1);
+		SDA_sig <= 'H';
+		
         wait; -- stop the process to avoid an infinite loop
 
 	end process slave_stimulus;
@@ -141,7 +176,7 @@ architecture behavior of TWI_testbench is
                  DONE_O => DONE_O_sig, -- done status signal
                  ERR_O  => ERR_O_sig,  -- error status
                  CLK    => clk_sig,    -- Input Clock
-                 SRST   => SRST_sig,  -- Reset
+                 SRST   => RESET_sig,  -- Reset
                  SDA    => SDA_sig,    --TWI SDA
                  SCL    => SCL_sig);   --TWI SCL
 											-- address write (write)
--- tempSensorReader.srcs/sources_1/imports/Downloads/ADT_CSM.vhd
+++ tempSensorReader.srcs/sources_1/imports/Downloads/ADT_CSM.vhd
@@ -8,7 +8,7 @@ entity ADT_CSM is
       RESET : in std_logic; 
       SRST : out std_logic;
       DATA_OUT : out std_logic_vector(15 downto 0); 
-      CLK : inout std_logic; 
+      CLK : in std_logic; 
       STB_I : out std_logic;
       MSG_I : out std_logic; 
       A_I : out std_logic_vector(7 downto 0);
@@ -21,79 +21,91 @@ end ADT_CSM;
 
 architecture Behavioral of ADT_CSM is
 
-	TYPE state_type IS (INIT, RESET_ACTIVATE, RESET_DEACTIVATE, WAIT_BUS_FREE, START_READ_OPERATION, WAIT_READ_DONE_MSB, LOAD_MSB, LOAD_LSB);
+	TYPE state_type IS (INIT, RESET_ACTIVATE, RESET_DEACTIVATE, WAIT_BUS_FREE, START_READ_OPERATION, WAIT_READ_DONE_MSB, WAIT_READ_DONE_LSB, LOAD_MSB, LOAD_LSB);
 	SIGNAL present_state, next_state : state_type;
 	signal counter : integer := 0;
+	signal reset_counter : std_logic := '0';
 	CONSTANT addrAD2 : std_logic_vector(6 downto 0) := "1001011";
 	CONSTANT read_bit : std_logic := '1';
 	CONSTANT write_bit : std_logic := '0'; 
 	
 	begin
-		clocked : process(clk, RESET)
+	
+		clocked : process(CLK, RESET)
 			begin
-				if RESET = '1' then
-					present_state <= INIT;
-					counter <= 0;
-				elsif rising_edge(clk) then
-					present_state <= next_state;
-					counter <= counter + 1;
+				if RESET = '1' OR START = '0' then
+				   counter <= 0;
+                   present_state <= INIT;
+				elsif rising_edge(CLK) then
+					if reset_counter = '1' then
+                         counter <= 0;
+                    else
+                         counter <= counter + 1;
+                     end if;
+                     present_state <= next_state;
 				end if;
 	end process clocked;
 	
 	
 	nextstate : PROCESS(present_state,counter)
 		BEGIN
+		  reset_counter <= '0';
 			CASE present_state IS
 				WHEN INIT =>
+				IF counter >= 10 THEN
 					next_state <= RESET_ACTIVATE;
+					reset_counter <= '1';
+				else
+				    next_state <= INIT;
+				END IF;	
 				WHEN RESET_ACTIVATE =>
-					IF counter >= 10 THEN
+					IF counter >= 2 THEN
 						next_state <= RESET_DEACTIVATE;
+						reset_counter <= '1';
 					ELSE
 						next_state <= RESET_ACTIVATE;
 					END IF;
 				WHEN RESET_DEACTIVATE =>
-					IF counter >= 2 THEN
+					IF counter >= 1200  THEN 
 						next_state <= WAIT_BUS_FREE;
+						reset_counter <= '1';
 					ELSE
 						next_state <= RESET_DEACTIVATE;
-					END IF;
+					END IF;	
 				WHEN WAIT_BUS_FREE =>
-					IF counter >= 1200  THEN
+					IF counter >= 1  THEN 
 						next_state <= START_READ_OPERATION;
+						reset_counter <= '1';
 					ELSE
 						next_state <= WAIT_BUS_FREE;
 					END IF;
+							
 				WHEN START_READ_OPERATION =>
-					IF counter >= 1  THEN
-						next_state <= WAIT_READ_DONE_MSB;
-					ELSE
-						next_state <= START_READ_OPERATION;
-					END IF;	
+				    IF DONE_O'event AND DONE_O = '0' THEN
+					        next_state <= WAIT_READ_DONE_MSB;
+					        reset_counter <= '1';
+					    else
+				             next_state <= START_READ_OPERATION;
+				         end if;
 				WHEN WAIT_READ_DONE_MSB =>
-					IF DONE_O'event AND DONE_O = '0' THEN
+				    IF counter >= 510  THEN
 						next_state <= LOAD_MSB;
-					ELSE
-						next_state <= WAIT_READ_DONE_MSB;
-					END IF;	
+						reset_counter <= '1';
+				    else
+				        next_state <= WAIT_READ_DONE_MSB;
+				    end if;
 				WHEN LOAD_MSB =>
-					IF counter >= 510 THEN
-						next_state <= LOAD_LSB;
-					ELSE
-						next_state <= LOAD_MSB;
-					END IF;
-				WHEN LOAD_LSB =>
-					IF DONE_O'event AND DONE_O = '0' THEN
-						next_state <= INIT;
-					ELSE
-						next_state <= LOAD_LSB;
-					END IF;
+					next_state <= WAIT_READ_DONE_LSB;
+				WHEN WAIT_READ_DONE_LSB =>
+				IF DONE_O'event AND DONE_O = '0' THEN
+				        next_state <= LOAD_LSB;
+					else 
+					   next_state <= WAIT_READ_DONE_LSB;
+					end if;			
 		        WHEN others => 
-		          
 			END CASE;
 	END PROCESS nextstate;
 
-	
 	output : PROCESS(present_state,counter)
 		BEGIN	
 			CASE present_state IS
@@ -101,8 +113,8 @@ architecture Behavioral of ADT_CSM is
 				    MSG_I <= '0';
 				    STB_I <= '0';
 				    SRST <= '0';
-				    D_I <= B"0000_0000";
 				    A_I <= addrAD2 & write_bit;
+				    D_I <= B"0000_0000";
 				WHEN RESET_ACTIVATE =>
 					SRST <= '1';
 				WHEN RESET_DEACTIVATE =>
@@ -117,9 +129,8 @@ architecture Behavioral of ADT_CSM is
 				    STB_I <= '0';
 					DATA_OUT(15 downto 8) <= D_O;
 				WHEN LOAD_LSB =>
-					DATA_OUT(7 downto 0) <= D_O;
+					DATA_OUT(7 downto 0) <= D_O;   
 			    WHEN OTHERS => 
-			    
 			END CASE;
 	END PROCESS output;
 	
